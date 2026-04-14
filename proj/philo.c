7fb1dc3a249059dab4393afdb94bfe878e93041e
ison <ison.c.2012.tk@gmail.com>
2026-04-14 13:55:58 +0900

    shape up

--- proj/philo.c
+++ proj/philo.c
@@ -6,7 +6,7 @@
 /*   By: keitotak <keitotak@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 17:36:01 by keitotak          #+#    #+#             */
-/*   Updated: 2026/04/14 13:34:45 by keitotak         ###   ########.fr       */
+/*   Updated: 2026/04/14 13:50:29 by keitotak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,7 @@ typedef struct s_philo
 void	*philo_routine(void *p)
 {
 	t_philo	*philo = (t_philo *)p;
-	int	left, right;
 
-	left = philo->id - 1;
-	if (left > 0)
-		right = left - 1;
-	else
-		right = philo->shared->nb_philo - 1;
-	philo->left_fork = philo->shared->forks[left];
-	philo->right_fork = philo->shared->forks[right];
 	printf("No.%d\n", philo->id);
 	return (NULL);
 }
@@ -48,6 +40,8 @@ int	philo(t_shared *shared)
 	{
 		philos[i].id = i + 1;
 		philos[i].shared = shared;
+		philos[i].left_fork = shared->forks[i];
+		philos[i].right_fork = i > 0 ? shared->forks[i - 1] : shared->forks[shared->nb_philo - 1];
 		if (pthread_create(&philos[i].th, NULL, &philo_routine, (void *)&philos[i]))
 			return (1);
 		i++;
