a41b767b7ac88cd1d7affe3b44f16dcfa21bd125
Ison Kislovodsk <ison.c.2012.tk@gmail.com>
2026-04-19 15:28:34 +0900

    add actions

--- proj/philo.c
+++ proj/philo.c
@@ -3,28 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: keitotak <keitotak@student.42tokyo.jp      +#+  +:+       +#+        */
+/*   By: keitotak <keitotak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/13 17:36:01 by keitotak          #+#    #+#             */
-/*   Updated: 2026/04/16 23:04:20 by keitotak         ###   ########.fr       */
+/*   Creatinged: 2026/04/13 17:36:01 by keitotak          #+#    #+#             */
+/*   Updated: 2026/04/19 15:15:28 by keitotak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <pthread.h>
 
+void	thinking(t_philo *p)
+{
+	printf("%d is thinking\n", p->id);
+//	usleep(1000);
+}
+
+void	pick_up_left_right(t_philo *p)
+{
+	pthread_mutex_lock(&p->left_fork);
+	printf("%d has taken a fork\n", p->id);
+	pthread_mutex_lock(&p->right_fork);
+	printf("%d has taken a fork\n", p->id);
+}
+
+void	pick_up_right_left(t_philo *p)
+{
+	pthread_mutex_lock(&p->right_fork);
+	printf("%d has taken a fork\n", p->id);
+	pthread_mutex_lock(&p->left_fork);
+	printf("%d has taken a fork\n", p->id);
+}
+
+void	eating(t_philo *p)
+{
+	printf("%d is eating spaghetti.\n", p->id);
+	usleep(p->shared->time_to_eat * 1000);
+}
+
+void	put_down_left_right(t_philo *p)
+{
+	pthread_mutex_unlock(&p->left_fork);
+	printf("%d has put down a fork\n", p->id);
+	pthread_mutex_unlock(&p->right_fork);
+	printf("%d has put down a fork\n", p->id);
+}
+
+void	put_down_right_left(t_philo *p)
+{
+	pthread_mutex_unlock(&p->right_fork);
+	printf("%d has put down a fork\n", p->id);
+	pthread_mutex_unlock(&p->left_fork);
+	printf("%d has put down a fork\n", p->id);
+}
+
+void	sleeping(t_philo *p)
+{
+	printf("%d is sleeping.\n", p->id);
+	usleep(p->shared->time_to_sleep * 1000);
+}
+
 void	*philo_routine(void *p)
 {
 	t_philo	*philo = (t_philo *)p;
 
-//	printf("No.%d\n", philo->id);
-	if (philo->id % 2 == 0)
+	while (1)
 	{
-
-	}
-	else
-	{
-
+		thinking(philo);
+		if (philo->id % 2 == 0)
+			pick_up_left_right(philo);
+		else
+			pick_up_right_left(philo);
+		eating(philo);
+		if (philo->id % 2 == 0)
+			put_down_left_right(philo);
+		else
+			put_down_right_left(philo);
+		sleeping(philo);
 	}
 	return (NULL);
 }
