ec7fa6512834887f9e742740fe5174ad069efd73
Ison Kislovodsk <ison.c.2012.tk@gmail.com>
2026-05-21 23:42:28 +0900

    update test shellscript

--- proj/philo/test/test.sh
+++ proj/philo/test/test.sh
@@ -3,22 +3,34 @@
 count=0
 readonly MAX_TRY=10
 
+if [ $# -eq 0 ]
+then
+	echo "Error: Give your arguments."
+	return 2> /dev/null || exit 1
+fi
+
+make
+echo ""
+echo "===TESTING PHILOSOPHERS==="
+echo "===TRY <./philo $@> IN $MAX_TRY TIMES==="
+echo "testing ..."
+
 while true
 do
-	./philo $@ > log
-
+	./philo $@ > log 2> err_log
 	count=$((count + 1))
 
 	if grep -q "died" log
 	then
+		echo -n "result: "
 		echo "died at try $count"
 		break
 	fi
 
 	if (( count == MAX_TRY ))
 	then
+		echo -n "result: "
 		echo "No philosopher died with $count try"
 		break
 	fi
-
 done
